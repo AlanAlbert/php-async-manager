8ad3c66e7a8e3be5fee496ba222b716d31d5e5e4
AlanAlbert <1766447919@qq.com>
2019-08-08 23:19:24 +0800

    【新增】查看异步任务

--- src/AsyncManager.php
+++ src/AsyncManager.php
@@ -6,7 +6,42 @@ use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Throwable;
 
 class AsyncManager extends Application
-{
+{   
+    /**
+     * PID文件存放目录
+     * 
+     * @var		string	PID_DIR           
+     */
+    const PID_DIR           = '/tmp/php-async-pid/';
+
+    /**
+     * PIDS文件，存各个守护程序的信息
+     * 
+     * @var		string	PIDS_FILE           
+     */
+    const PIDS_FILE         = 'php-async.pids';        
+
+    /**
+     * 进程运行状态：运行中
+     * 
+     * @var		string	STATUS_RUNNING      
+     */
+    const STATUS_RUNNING    = 'RUNNING';  
+
+    /**
+     * 进程运行状态：已停止
+     * 
+     * @var		string	STATUS_STOPPED      
+     */
+    const STATUS_STOPPED    = 'STOPPED';                
+    
+    /**
+     * 进程运行状态：已完成
+     * 
+     * @var		string	STATUS_COMPLETED    
+     */
+    const STATUS_COMPLETED  = 'COMPLETED';  
+
     protected $commands = [
         ListCommand::class,     // 查看异步进程
         StopCommand::class,     // 停止异步进程
@@ -23,5 +58,8 @@ class AsyncManager extends Application
             }
             $this->add($instance);
         }
+
+        // 设置默认命令为help
+        $this->setDefaultCommand('help');
     }
 }
--- src/ListCommand.php
+++ src/ListCommand.php
@@ -5,20 +5,134 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Exception;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
 
 class ListCommand extends Command
 {
+    /**
+     * 配置list命令
+     *
+     * @return void
+     */
     protected function configure()
     {
         $this->setName('list')
             ->setAliases(['ls', 'l'])
-            ->addOption('filter', 'f', InputOption::VALUE_OPTIONAL, 'The option allows [<info>a</info>(ll), <info>r</info>(unning), <info>s</info>(topped), <info>c</info>(ompleted)]')
+            ->addOption('all', 'a', InputOption::VALUE_NONE, 'When the option is enabled, list all async process info.')
+            ->addOption('running', 'r', InputOption::VALUE_NONE, 'List running async process info.')
+            ->addOption('stopped', 's', InputOption::VALUE_NONE, 'List stopped async process info.')
+            ->addOption('completed', 'c', InputOption::VALUE_NONE, 'List completed async process info.')
             ->setDescription('list async jobs')
             ->setHelp('The command allows you to get this async jobs');
     }
 
+    /**
+     * list命令具体逻辑
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        var_dump($input->getOptions());
+        $filterOptions = $input->getOptions();
+        if ($filterOptions['running']) {
+            $asyncs = $this->parsePidsFile(AsyncManager::STATUS_RUNNING);
+            $this->render($asyncs, $output);
+        } elseif ($filterOptions['stopped']) {
+            $asyncs = $this->parsePidsFile(AsyncManager::STATUS_STOPPED);
+            var_dump($asyncs);
+        } elseif ($filterOptions['completed']) {
+            $asyncs = $this->parsePidsFile(AsyncManager::STATUS_COMPLETED);
+            var_dump($asyncs);
+        } else {
+            $asyncs = $this->parsePidsFile();
+            $this->render($asyncs, $output);
+        }
+
+        // 如果开启all, 覆盖以上
+        if ($filterOptions['all']) {
+            $asyncs = $this->parsePidsFile();
+            $this->render($asyncs, $output);
+        }
+
+    }
+
+    /**
+     * 解析pids文件
+     *
+     * @param string $type
+     * @return void
+     */
+    private function parsePidsFile($type = 'all')
+    {
+        $pidsFile = AsyncManager::PID_DIR . AsyncManager::PIDS_FILE;
+        if (!is_file($pidsFile)) {
+            return [];
+        }
+        $asyncs = json_decode(file_get_contents($pidsFile), true);
+        if ($asyncs === false) {
+            throw new Exception('打开文件' . $pidsFile . '失败(可能权限不足)');
+        }
+
+        // 返回全部
+        if ($type === 'all') {
+            return $asyncs;
+        }
+
+        // 过滤不符合参数状态的数据
+        $asyncs = array_filter($asyncs, function ($async) use ($type) {
+            return $async['status'] == $type;
+        });
+
+        return $asyncs;
+    }
+
+    /**
+     * 渲染
+     *
+     * @param array $asyncs
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    private function render($asyncs, OutputInterface $output)
+    {
+        // 重构数据
+        $rows = [];
+        foreach ($asyncs as $pid => $async) {
+            // 为不同状态加上颜色
+            switch ($async['status']) {
+                case AsyncManager::STATUS_RUNNING:
+                    $async['status'] = '<comment>'.$async['status'].'</comment>';
+                    break;
+                case AsyncManager::STATUS_STOPPED:
+                    $async['status'] = '<error>'.$async['status'].'</error>';
+                    break;
+                case AsyncManager::STATUS_COMPLETED:
+                    $async['status'] = '<info>'.$async['status'].'</info>';
+                    break;
+                default: 
+                    break;
+            }
+
+            $rows[] = [
+                'pid'           => $pid,
+                'status'        => $async['status'],
+                'start_time'    => $async['start_time'],
+                'end_time'      => $async['end_time']
+            ];
+        }
+        
+        // 渲染到表格
+        $table = new Table($output);
+        $table->setHeaderTitle('Async Tasks Info');
+        $table->setFooterTitle('Async Tasks Info');
+        $table->setHeaders(['PID', 'STATUS', 'START_TIME', 'END_TIME'])
+            ->addRows($rows);
+        $table->render();
     }
 }
